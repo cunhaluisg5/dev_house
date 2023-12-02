2474431414587759fc02bd7e46066798ae84ee75
Luís Cipriani <cunhaluisg5@gmail.com>
2023-12-01 21:18:59 -0300

    Adiciona conexão com mongodb

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import routes from './routes.js';
 
 class App{
   constructor(){
     this.server = express();
+
+    mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.hxehoiv.mongodb.net/?retryWrites=true&w=majority');
+
     this.middleware();
     this.routes();
   };
