e15643dbc467d3529b911d8a1de86e884e1c6ff2
Luís Cipriani <cunhaluisg5@gmail.com>
2023-12-06 20:08:29 -0300

    Edita e exclui

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,16 @@
 import House from '../models/House.js';
+import User from '../models/User.js';
 
 class HouseController{
+
+  async index(req, res){
+    const { status } = req.query;
+
+    const houses = await House.find({ status });
+
+    return res.json(houses);
+  }
+
   async store(req, res) {
     const { filename } = req.file;
     const { description, price, location, status } = req.body;
@@ -15,6 +25,46 @@ class HouseController{
       status
     });
     return res.json(house);
+  }
+
+  async update(req, res){
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if(String(user._id) !== String(houses.user)){
+      return res.status(401).json({ error: 'Não autorizado.'});
+    }
+
+    await House.updateOne({ _id: house_id }, {
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status
+    });
+    return res.send();
+  }
+
+  async destroy(req, res){
+    const { house_id } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if(String(user._id) !== String(houses.user)){
+      return res.status(401).json({ error: 'Não autorizado.'});
+    }
+
+    await House.findByIdAndDelete({ _id: house_id });
+
+    return res.json({ message: 'Excluído com sucesso!' });
   }
 };
 
