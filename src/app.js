8f1a99ddf5a0e447686e65b6637183411923f6ae
Luís Cipriani <cunhaluisg5@gmail.com>
2023-12-05 21:41:41 -0300

    Cadastra casa

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import routes from './routes.js';
 
@@ -14,6 +19,10 @@ class App{
   };
 
   middleware(){
+    this.server.use(
+      '/files', 
+      express.static(path.resolve(__dirname, '..', 'uploads'))
+    );
     this.server.use(express.json());
   }
 
---
+++ src/config/upload.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      const name = path.basename(file.originalname, ext);
+
+      cb(null, `${name}-${Date.now()}${ext}`);
+    }
+  })
+}
